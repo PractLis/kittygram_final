4f825ab983385eccee35d81484c4a8e54f5db511
Lilev Sergey <Lisser2006@yandex.ru>
2023-07-17 23:37:29 +0300

    ревью1

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,11 +7,11 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
-
-DEBUG = bool(strtobool(os.getenv('DEBUG', 'False')))
-
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(', ')
+SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$' 
+ 
+DEBUG = False 
+ 
+ALLOWED_HOSTS = ['158.160.15.224', '127.0.0.1', 'localhost', 'kittyspace.myftp.org']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
